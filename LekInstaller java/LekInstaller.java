b4335a79b7f936fa226220a0a6207aae5663c6c1
Nicolas Campion <44935471+Sitheau@users.noreply.github.com>
2021-12-21 07:59:10 -0500

    changes from mech installer
    added move function as well

--- LekInstaller java/LekInstaller.java
+++ LekInstaller java/LekInstaller.java
@@ -2,19 +2,24 @@ import java.net.*;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.nio.channels.*;
-import java.util.*;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipFile;
+import java.util.Enumeration;
 
 public class LekInstaller {
-
     public static void install() { //install to preset directory
         try {
-            URL assetsUrl = new URL("https://github.com/mech-lang/mech/releases/latest/download/mech_0.0.5_windows_x86_64.7z"); //TODO: change to generic file name -montella end
+            URL assetsUrl = new URL("https://github.com/EnormousApplePie/Lekmod/releases/latest/download/assets.zip"); //This link is the github the downloader targets /Author/repo name/releases
             HttpURLConnection connection = (HttpURLConnection) assetsUrl.openConnection();
             connection.setRequestProperty("Accept", "application/octet-stream");
             ReadableByteChannel uChannel = Channels.newChannel(connection.getInputStream());
-            String userDir = System.getProperty("user.home") + "\\Downloads";
-            FileOutputStream foStream = new FileOutputStream(userDir + "\\mech_0.0.5_windows_x86_64.7z"); //default installs to user downloads
+            String userDir = System.getenv("ProgramFiles(X86)") + "\\Steam\\steamapps\\common\\Sid Meier's Civilization V\\Assets";;
+            FileOutputStream foStream = new FileOutputStream(userDir + "\\assets.zip");
             FileChannel fChannel = foStream.getChannel();
             fChannel.transferFrom(uChannel, 0, Long.MAX_VALUE);
             uChannel.close();
@@ -26,7 +31,7 @@ public class LekInstaller {
  
     }
 
-    public static void delete(File folder) throws IOException{ //recursively delete folders contents then delete folder, be careful :^)
+    public static void delete(File folder) throws IOException{ //recursively delete folders contents then delete folder in documents, be careful :^)
         if (folder.isDirectory()) {
             for (File sub : folder.listFiles()) {
                 delete(sub);
@@ -35,16 +40,57 @@ public class LekInstaller {
         folder.delete();
     }
 
-    public static void unpack() throws IOException { //TODO: unzip installed version and delete zip
-       
+    private static void move(String targetPath, String destPath, String oldVer) throws IOException {
+        File fileToMove = new File(targetPath);
+        File fileDest = new File(destPath+"\\"+oldVer);
+        Files.move(fileToMove.toPath(), fileDest.toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 
-    public static void build()  { //TODO: run mech.exe and any other commands in it to install
+    private static void unpack(String zipFilePath, String destDir) throws IOException { //unzips target at zipFilePath into destDir, making a folder
+        // Open the zip file
+        ZipFile zipFile = new ZipFile(zipFilePath);
+        Enumeration<?> enu = zipFile.entries();
+        while (enu.hasMoreElements()) {
+            ZipEntry zipEntry = (ZipEntry) enu.nextElement();
 
+            String name = zipEntry.getName();
+            long size = zipEntry.getSize();
+            long compressedSize = zipEntry.getCompressedSize();
+            System.out.printf("name: %-20s | size: %6d | compressed size: %6d\n", 
+                    name, size, compressedSize);
+
+            // Do we need to create a directory ?
+            File file = new File(System.getenv("ProgramFiles(X86)") + "\\Steam\\steamapps\\common\\Sid Meier's Civilization V\\Assets\\"+name);
+            if (name.endsWith("/")) {
+                file.mkdirs();
+                continue;
+            }
+
+            File parent = file.getParentFile();
+            if (parent != null) {
+                parent.mkdirs();
+            }
+
+            // Extract the file
+            InputStream is = zipFile.getInputStream(zipEntry);
+            FileOutputStream fos = new FileOutputStream(file);
+            byte[] bytes = new byte[1024];
+            int length;
+            while ((length = is.read(bytes)) >= 0) {
+                fos.write(bytes, 0, length);
+            }
+            is.close();
+            fos.close();
+
+        }
+        zipFile.close();
+        File old_zipper = new File(zipFilePath); //kills old zipper :<
+        delete(old_zipper);
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        URL tagUrl = new URL("https://api.github.com/repos/mech-lang/mech/tags"); //url for all tags
+        URL tagUrl = new URL("https://api.github.com/repos/EnormousApplePie/Lekmod/tags"); //url for all tags in the repo
+        String civAssetsPath = System.getenv("ProgramFiles(X86)") + "\\Steam\\steamapps\\common\\Sid Meier's Civilization V\\Assets";
 
         HttpURLConnection c = (HttpURLConnection) tagUrl.openConnection(); //GET JSON for all tags
         c.setRequestMethod("GET");
@@ -61,33 +107,48 @@ public class LekInstaller {
                 StringBuilder sb = new StringBuilder();
                 sb.append(br.readLine());
                 br.close();
-
-                String[] vTemp = sb.toString().split("\"", 5); //weird regex to get the latest version 
-                String[] verNum = vTemp[3].split("v0."); //get just the last 2 numbers out of v0.x.x
-                double verDouble = Double.parseDouble(verNum[1].trim()); //convernt to double to compare
-
-
-                //TODO: check if there is a local ver that exists if not skip ver check
-                if(verDouble > 0.4) { //TODO: replace 0.4 with read local version
-                    String mechPath = System.getProperty("user.home") + "\\Downloads\\mech";
-                    File mechFolder = new File(mechPath); //assign var to mech folder location
-
-                    delete(mechFolder); //careful with testing, needs mechFolder to already exist
-                    install();
-                    unpack();
-                    build();
-                    System.out.println("swag");
-                } else {
-                    System.out.println("You are up to date :^)");
-                }
-               
+                //funky string manipulation to get the first tag out of the response of ALL tags, scrapes the first vX.X.X tag in the response as the latest version
+                String[] vTemp = sb.toString().split("v"); //weird regex to get the latest version
+                String[] verNum = vTemp[15].split("\""); //change vTemp[15] to not include vdev tags
                 
+                double verDouble = Double.parseDouble(verNum[0].trim()); //convernt to double to compare
+                
+                double localModVer = 29.1;
+                double localMapVer = 3.4; //civAssetsPath+"\\Maps\\" search for folder with Lekmap+" " starter
+
+                String localLekMod = "LEKMOD_v" + Double.toString(localModVer);
+                String localLekMap = "Lekmap v" + Double.toString(localMapVer);
+
+                File modDirectory = new File(civAssetsPath+"\\DLC\\"+localLekMod); //check if file exists
+                File mapDirectory = new File(civAssetsPath+"\\Maps\\"+localLekMap); //check if file exists
+                boolean tempExists = modDirectory.exists() && mapDirectory.exists();
+
+                System.out.println("latest ver " + verDouble);
+
+                if(tempExists) {    //check if that directory already exists (if yes need to update)
+                    if(verDouble > localModVer) { //ONLY checks if LEKMOD is out of date not map, would need 2nd tag bleh
+                        File civFolder = new File(civAssetsPath); //assign var to mech folder location
+                        System.out.println("updating");
+                        
+                        install(); //add check for successful install
+                        unpack(civAssetsPath+"\\assets.zip", civAssetsPath);
+                        //delete(civFolder); //DELETE OLD
+                        move(civAssetsPath+"\\"+localLekMod, civAssetsPath+"\\DLC\\", localLekMod); //move mod folder
+                        move(civAssetsPath+"\\"+localLekMap, civAssetsPath+"\\Maps", localLekMap); //move map folder
+                        System.out.println("success");
+                    } else {
+                        System.out.println("You are up to date :^)");
+                    }
+                } else { //first time set up
+                    System.out.println("installing first time");
+
+                    install(); //add check for successful install
+                    unpack(civAssetsPath+"\\assets.zip", civAssetsPath);
+                    move(civAssetsPath+"\\"+localLekMod, civAssetsPath+"\\DLC\\", localLekMod); //move mod folder
+                    move(civAssetsPath+"\\"+localLekMap, civAssetsPath+"\\Maps", localLekMap); //move map folder
+                    System.out.println("success");
+                }
+                   
         }
     }
 }
-
-//TODO: GUI lets user specify install location
-//TODO: changes for lek version
-//      change install location (need to move folders into steam ones)
-//      change github sided version detector to whatever tag system EAP will use
-//      either get EAP to make releases in 7zip or go back to native java unzipper
